6a538bcdea96f86065c35b078a6043535a4707fe
Max Kuhn <mxkuhn@gmail.com>
2017-04-25 10:26:20 -0400

    Monte Carlo CV

---
+++ R/mc.R
@@ -0,0 +1,50 @@
+#' Monte Carlo Cross-Validation
+#'
+#' @inheritParams vfold_cv
+#' @param prop The proportion of data to be retained for modeling/analysis. 
+#' @param times The number of times to repeat the sampling.. 
+#' @param strata A variable that is used to conduct stratified sampling to create the resamples.  
+#' @export
+#' @return  An object with classes \code{"mc_cv"} and \code{"rset"}. The elements of the object include a tibble called \code{splits} that contains a column for the data split objects and a column called \code{id} that has a character string with the resample identifier.
+#' @examples
+#' mc_cv(mtcars, times = 2)
+#' mc_cv(mtcars, prop = .5, times = 2)
+#' @export
+mc_cv <- function(data, prop = 3/4, times = 25, strata = NULL, ...) {
+  split_objs <- mc_splits(data = data, prop = 1 - prop, times = times)
+  
+  structure(list(splits = split_objs, 
+                 prop = prop, strata = strata, 
+                 times = times), 
+            class = c("mc_cv", "rset"))
+}
+
+# Get the indices of the analysis set from the assessment set
+mc_complement <- function(ind, n) {
+  list(analysis = setdiff(1:n, ind),
+       assessment = ind)
+}
+
+mc_splits <- function(data, prop = 3/4, times = 25) {
+  if (!is.numeric(prop) | prop >= 1 | prop <= 0) 
+    stop("`prop` must be a number on (0, 1).", call. = FALSE)
+  
+  n <- nrow(data)
+  indices <- purrr::map(rep(n, times), sample, size = floor(n*prop))
+  indices <- lapply(indices, mc_complement, n = n)
+  
+  split_objs <- purrr::map(indices, make_splits, data = data)
+  tibble::tibble(splits = split_objs, 
+                 id = paste0("Resample", seq_along(split_objs)))
+}
+
+#' @export
+print.mc_cv <- function(x, ...) {
+  cat(
+    "Monte Carlo cross-validation (",
+    signif(x$prop, 2), "/", 
+    signif(1-x$prop, 2), 
+    ") with ", x$times, " resamples ", sep = "")
+  if(!is.null(x$strata)) cat("using stratification")
+  cat("\n")
+}
