bac60b019b91e768b4ad8409d132355fb8eaa85b
Max Kuhn <mxkuhn@gmail.com>
2017-05-23 10:27:02 -0400

    Removed dim.rset since it screws up the tibble

--- R/misc.R
+++ R/misc.R
@@ -5,49 +5,19 @@ make_splits <- function(ind, data, class = NULL) {
   res
 }
 
-get_rsplit <- function(object, id = object$splits$id[1]) {
-  if(length(id) > 1 || !is.character(id))
-    stop("`id` should be a single character value.")
-  ind <- which(id == object$splits$id)
-  if(length(ind) == 0)
-    stop("That `id` value was not found", call. = FALSE)
-  getElement(object$splits, "splits")[ind][[1]]
-}
-
 merge_lists <- function(a, b) list(analysis = a, assessment = b)
 
-#' @export
-dim.rset <- function(x, ...) {
-  dims <- purrr::map(x$splits$splits, dim)
-  # dims <- dplyr::bind_rows(dims) # seg faults
+dim_rset <- function(x, ...) {
+  dims <- purrr::map(x$splits, dim)
   dims <- do.call("rbind", dims)
   dims <- tibble::as_tibble(dims)
-  dims <- cbind(dims, x$splits[, grepl("^id", colnames(x$splits))])
+  id_cols <- grep("^id", colnames(x), value = TRUE)
+  for(i in seq_along(id_cols))
+    dims[id_cols[i]] <- getElement(x, id_cols[i])
   dims
 } 
+## Note, column subsetting doesn't work with expanded classes
 
-
-#' Extract the resampling information
-#' 
-#' The convenience function can be used to extract the data structure that contains the resampling splits or a specific element of that structure. 
-#' @param x An `rset` object
-#' @param  .elem A character value or \code{NULL}. If \code{NULL}, then the entire tibble is returned. Values of the argument can be any column in the tibble.  
-#' @return A tibble, vector, or list depending on the value of \code{.elem}. 
-#' @examples  
-#' set.seed(36522)
-#' bt <- bootstraps(mtcars, times = 3)
-#' splits(bt)
-#' splits(bt, "id")
-#' splits(bt, "splits")
-#' @export
-splits <- function(x, .elem = NULL) {
-  if(!is.null(.elem)) {
-    if(!.elem %in% names(x$splits))
-      stop("`", .elem, "` is not in the `splits` tibble", call. = FALSE)
-    res <- x$splits[[.elem]]
-  } else res <- x$splits
-  res
-}
 
 names0 <- function (num, prefix = "x") {
   if (num < 1) 
