657a4e2fe2ed9aada8a998257b879a6f32404123
Max Kuhn <max@imp.local>
2018-01-08 20:16:24 -0500

    minor documentation update

--- R/rsplit.R
+++ R/rsplit.R
@@ -1,19 +1,19 @@
 rsplit <- function(data, in_id, out_id) {
   if (!is.data.frame(data) & !is.matrix(data))
     stop("`data` must be a data frame.", call. = FALSE)
-  
+
   if (!is.integer(in_id) | any(in_id < 1))
     stop("`in_id` must be a positive integer vector.", call. = FALSE)
-  
+
   if(!all(is.na(out_id))) {
     if (!is.integer(out_id) | any(out_id < 1))
       stop("`out_id` must be a positive integer vector.", call. = FALSE)
   }
-  
+
   if (length(in_id) == 0)
-    stop("At least one row should be selected for the analysis set.", 
+    stop("At least one row should be selected for the analysis set.",
          call. = FALSE)
-  
+
   structure(
     list(
       data = data,
@@ -31,7 +31,7 @@ print.rsplit <- function(x, ...) {
       paste(length(complement(x)))
   else
     paste(length(x$out_id))
-  
+
   cat("<",
       length(x$in_id), "/",
       out_char, "/",
@@ -40,7 +40,7 @@ print.rsplit <- function(x, ...) {
 }
 
 #' @export
-as.integer.rsplit <- 
+as.integer.rsplit <-
   function(x, data = c("analysis", "assessment"), ...) {
     data <- match.arg(data)
     if (data == "analysis")
@@ -56,16 +56,23 @@ as.integer.rsplit <-
 
 
 #' Convert an `rsplit` object to a data frame
-#' 
+#'
 #' The analysis or assessment code can be returned as a data
 #'   frame (as dictated by the `data` argument) using
-#'   `as.data.frame.rsplit`. `analysis` and 
-#'   `assessment` are shortcuts. 
+#'   `as.data.frame.rsplit`. `analysis` and
+#'   `assessment` are shortcuts.
 #' @param x An `rsplit` object.
 #' @param row.names `NULL` or a character vector giving the row names for the data frame. Missing values are not allowed.
 #' @param optional A logical: should the column names of the data be checked for legality?
-#' @param data Either "analysis" or "assessment" to specify which data are returned. 
-#' @param ...	Additional arguments to be passed to or from methods. Not currently used. 
+#' @param data Either "analysis" or "assessment" to specify which data are returned.
+#' @param ...	Additional arguments to be passed to or from methods. Not currently used.
+#' @examples
+#' library(dplyr)
+#' set.seed(104)
+#' folds <- vfold_cv(mtcars)
+#'
+#' model_data_1 <- folds$splits[[1]] %>% analysis()
+#' holdout_data_1 <- folds$splits[[1]] %>% assessment()
 #' @export
 as.data.frame.rsplit <-
   function(x,
@@ -73,23 +80,23 @@ as.data.frame.rsplit <-
            optional = FALSE,
            data = "analysis",
            ...) {
-    
+
   if (!is.null(row.names))
     warning( "`row.names` is kept for consistency with the ",
-             "underlying class but non-NULL values will be ", 
+             "underlying class but non-NULL values will be ",
              "ignored.", call. = FALSE)
   if (optional)
     warning( "`optional` is kept for consistency with the ",
-             "underlying class but TRUE values will be ", 
+             "underlying class but TRUE values will be ",
              "ignored.", call. = FALSE)
   x$data[as.integer(x, data = data, ...), , drop = FALSE]
 }
 
-#' @rdname as.data.frame.rsplit  
+#' @rdname as.data.frame.rsplit
 #' @export
 analysis <- function(x, ...)
   as.data.frame(x, data = "analysis", ...)
-#' @rdname as.data.frame.rsplit  
+#' @rdname as.data.frame.rsplit
 #' @export
 assessment <- function(x, ...)
   as.data.frame(x, data = "assessment", ...)
@@ -114,7 +121,7 @@ obj_sum.rsplit <- function(x, ...) {
       paste(length(complement(x)))
   else
     paste(length(x$out_id))
-  
+
   paste0("rsplit [",
          length(x$in_id), "/",
          out_char, "/",
