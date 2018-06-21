d6469502d3eae205761d751dfa5634ce0c7d83f2
topepo <mxkuhn@gmail.com>
2018-06-21 09:53:16 -0400

    added a new data set

--- R/data.R
+++ R/data.R
@@ -22,3 +22,20 @@
 #' data(attrition)
 #' str(attrition)
 NULL
+
+
+#' Sample Two Class Data
+#'
+#' @details There are artifical data with two predictors (`A` and `B`) and
+#'  a factor outcome variable (`Class`).
+#'
+#' @name two_class_dat
+#' @aliases two_class_dat
+#' @docType data
+#' @return \item{two_class_dat}{a data frame}
+#'
+#' @keywords datasets
+#' @examples
+#' data(two_class_dat)
+#' str(two_class_dat)
+NULL
