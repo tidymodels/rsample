a8eea21e19321dc2f940506ca29396f815c44677
DavisVaughan <mvaugh15@uncc.edu>
2018-07-12 21:55:52 -0400

    Add drinks dataset

--- R/data.R
+++ R/data.R
@@ -39,3 +39,22 @@ NULL
 #' data(two_class_dat)
 #' str(two_class_dat)
 NULL
+
+#' Sample Time Series Data
+#'
+#' @details Drink sales. The exact name of the series from FRED is:
+#' "Merchant Wholesalers, Except Manufacturers' Sales Branches and Offices
+#' Sales: Nondurable Goods: Beer, Wine, and Distilled Alcoholic Beverages Sales"
+#'
+#' @name drinks
+#' @aliases drinks
+#' @docType data
+#' @return \item{drinks}{a data frame}
+#'
+#' @source The Federal Reserve Back of St. Louis website https://fred.stlouisfed.org/series/S4248SM144NCEN
+#'
+#' @keywords datasets
+#' @examples
+#' data(drinks)
+#' str(drinks)
+NULL
