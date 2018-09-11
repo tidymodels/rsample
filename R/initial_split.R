760d16a0ce6876761f17b069c283b3933cc6bcb9
Clayton Yochum <claytonjy@gmail.com>
2018-09-10 23:24:23 -0400

    Add new initial_time_split function.

--- R/initial_split.R
+++ R/initial_split.R
@@ -1,24 +1,25 @@
 #' Simple Training/Test Set Splitting
 #'
-#' `initial_split` creates a single binary split of the data 
-#'  into a training set and testing set. `training` and
-#'  `testing` are used to extract the resulting data.
+#' `initial_split` creates a single binary split of the data into a training set
+#' and testing set. `initial_split` does the same, but takes the _first_ `prop`
+#' samples for training, instead of a a random selection. `training` and
+#' `testing` are used to extract the resulting data.
 #'
-#' @details 
-#' The `strata` argument causes the random sampling to be conducted *within the stratification variable*. The can help ensure that the number of data points in the training data is equivalent to the proportions in the original data set.  
+#' @details
+#' The `strata` argument causes the random sampling to be conducted *within the stratification variable*. The can help ensure that the number of data points in the training data is equivalent to the proportions in the original data set.
 #'
 #' @inheritParams vfold_cv
-#' @param prop The proportion of data to be retained for modeling/analysis. 
-#' @param strata A variable that is used to conduct stratified sampling to create the resamples. 
+#' @param prop The proportion of data to be retained for modeling/analysis.
+#' @param strata A variable that is used to conduct stratified sampling to create the resamples.
 #' @export
-#' @return  An `rset` object that can be used with the `training` and `testing` functions to extract the data in each split. 
+#' @return  An `rset` object that can be used with the `training` and `testing` functions to extract the data in each split.
 #' @examples
 #' set.seed(1353)
 #' car_split <- initial_split(mtcars)
 #' train_data <- training(car_split)
-#' test_data <- testing(car_split) 
+#' test_data <- testing(car_split)
 #' @export
-#' 
+#'
 initial_split <- function(data, prop = 3/4, strata = NULL, ...) {
   res <-
     mc_cv(
@@ -31,12 +32,23 @@ initial_split <- function(data, prop = 3/4, strata = NULL, ...) {
   res$splits[[1]]
 }
 
-#' @rdname initial_split 
+#' @rdname initial_split
+#' @export
+initial_time_split <- function(data, prop = 3/4, ...) {
+
+  if (!is.numeric(prop) | prop >= 1 | prop <= 0) {
+    stop("`prop` must be a number on (0, 1).", call. = FALSE)
+  }
+
+  n_train <- floor(nrow(data) * prop)
+
+  rsplit(data, 1:n_train, (n_train + 1):nrow(data))
+}
+
+#' @rdname initial_split
 #' @export
 #' @param x An `rsplit` object produced by `initial_split`
 training <- function(x) analysis(x)
-#' @rdname initial_split 
+#' @rdname initial_split
 #' @export
 testing <- function(x) assessment(x)
-
-  
--- tests/testthat/test_initial.R
+++ tests/testthat/test_initial.R
@@ -14,3 +14,12 @@ test_that('default param', {
   expect_equal(nrow(ts1), nrow(dat1)/4)
 })
 
+test_that('default time param', {
+  rs1 <- initial_time_split(dat1)
+  expect_equal(class(rs1), "rsplit")
+  tr1 <- training(rs1)
+  ts1 <- testing(rs1)
+  expect_equal(nrow(tr1), floor(nrow(dat1) * 3/4))
+  expect_equal(nrow(ts1), ceiling(nrow(dat1) / 4))
+  expect_equal(tr1, dplyr::slice(dat1, 1:floor(nrow(dat1) * 3/4)))
+})
