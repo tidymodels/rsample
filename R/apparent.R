cebed0267d2710878232c48d793309022af037ea
fbchow <fannybchow@gmail.com>
2019-01-25 11:04:33 -0800

    Small typo

--- R/apparent.R
+++ R/apparent.R
@@ -1,17 +1,17 @@
 #' Sampling for the Apparent Error Rate
 #'
 #' When building a model on a data set and re-predicting the same data, the
-#'   performance estimate from those predictions is often call the 
-#'   "apparent" performance of the model. This estimate can be wildly 
-#'   optimistic. "Apparent sampling" here means that the analysis and 
-#'   assessment samples are the same. These resamples are sometimes used in 
+#'   performance estimate from those predictions is often call the
+#'   "apparent" performance of the model. This estimate can be wildly
+#'   optimistic. "Apparent sampling" here means that the analysis and
+#'   assessment samples are the same. These resamples are sometimes used in
 #'   the analysis of bootstrap samples and should otherwise be
-#'   avoided like ol sushi. 
+#'   avoided like old sushi.
 #'
 #' @inheritParams vfold_cv
-#' @return  An tibble with a single row and classes `apparent`, 
-#'   `rset`, `tbl_df`, `tbl`, and `data.frame`. The 
-#'   results include a column for the data split objects and one column 
+#' @return  A tibble with a single row and classes `apparent`,
+#'   `rset`, `tbl_df`, `tbl`, and `data.frame`. The
+#'   results include a column for the data split objects and one column
 #'   called `id` that has a character string with the resample identifier.
 #' @examples
 #' apparent(mtcars)
@@ -22,12 +22,12 @@ apparent <- function(data, ...) {
   # splits <- rm_out(splits)
   class(splits) <- c("rsplit", "apparent_split")
   split_objs <- tibble::tibble(splits = list(splits), id = "Apparent")
-  
+
   split_objs <-
     add_class(split_objs,
               cls = c("apparent", "rset"),
               at_end = FALSE)
-  
+
   split_objs
 }
 
