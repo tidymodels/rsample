4a06bdb77e22253c607d89b248f18713afeee73f
topepo <mxkuhn@gmail.com>
2019-07-03 12:38:00 -0400

    closes #93

--- R/lables.R
+++ R/lables.R
@@ -40,12 +40,13 @@ labels.vfold_cv <- function(object, make_factor = FALSE, ...) {
 
 #' Find Labels from rsplit Object
 #'
-#' Produce a tibble of identification variables so that single 
-#'  splits can be linked to a particular resample. 
+#' Produce a tibble of identification variables so that single
+#'  splits can be linked to a particular resample.
 #'
 #' @param object An `rsplit` object
 #' @param ... Not currently used.
 #' @return A tibble.
+#' @seealso add_resample_id
 #' @export
 #' @examples
 #' cv_splits <- vfold_cv(mtcars)
@@ -182,5 +183,55 @@ pretty.bootstraps <- function(x, ...) {
 pretty.group_vfold_cv  <- function(x, ...) {
   details <- attributes(x)
   paste0("Group ", details$v, "-fold cross-validation")
+}
+
+
+#' Augment a data set with resampling identifiers
+#'
+#' For a data set, `add_resample_id()` will add at least one new column that
+#'  identifies which resample that the data came from. In most cases, a single
+#'  column is added but for some resampling methods two or more are added.
+#' @param .data A data frame
+#' @param split A single `rset` object.
+#' @param dots A single logical: should the id columns be prefixed with a "."
+#'  to avoid name conflicts with `.data`?
+#' @return An updated data frame.
+#' @examples
+#'
+#' set.seed(363)
+#' car_folds <- vfold_cv(mtcars, repeats = 3)
+#'
+#' analysis(car_folds$splits[[1]]) %>%
+#'   add_resample_id(car_folds$splits[[1]]) %>%
+#'   head()
+#'
+#' car_bt <- bootstraps(mtcars)
+#'
+#' analysis(car_bt$splits[[1]]) %>%
+#'   add_resample_id(car_bt$splits[[1]]) %>%
+#'   head()
+#' @seealso labels.rsplit
+#' @export
+add_resample_id <- function(.data, split, dots = FALSE) {
+  if (!inherits(dots, "logical") || length(dots) > 1) {
+    stop("`dots` should be a single logical.", call. = FALSE)
+  }
+  if (!inherits(.data, "data.frame")) {
+    stop("`.data` should be a data frame.", call. = FALSE)
+  }
+  if (!inherits(split, "rsplit")) {
+    stop("`split` should be a single 'rset' object.", call. = FALSE)
+  }
+  labs <- labels(split)
+
+  if (!tibble::is_tibble(labs) && nrow(labs) == 1) {
+    stop("`split` should be a single 'rset' object.", call. = FALSE)
+  }
+
+  if (dots) {
+    colnames(labs) <- paste0(".", colnames(labs))
+  }
+
+  cbind(.data, labs)
 }
 
--- tests/testthat/test_labels.R
+++ tests/testthat/test_labels.R
@@ -1,7 +1,7 @@
-context("Labels")
-
 library(testthat)
 library(rsample)
+
+context("Labels")
 
 test_that('basic cv', {
   cv_obj <- vfold_cv(mtcars)
@@ -27,4 +27,46 @@ test_that('nested cv', {
                 )
       )
     )
+})
+
+test_that('adding labels', {
+  set.seed(363)
+  car_folds <- vfold_cv(mtcars, repeats = 3)
+
+  res <-
+    analysis(car_folds$splits[[1]]) %>%
+    add_resample_id(car_folds$splits[[1]])
+  expect_equal(colnames(res), c(colnames(mtcars), "id", "id2"))
+
+  car_bt <- bootstraps(mtcars)
+
+  res <- analysis(car_bt$splits[[1]]) %>%
+    add_resample_id(car_bt$splits[[1]])
+
+  expect_equal(colnames(res), c(colnames(mtcars), "id"))
+
+  res <- analysis(car_bt$splits[[1]]) %>%
+    add_resample_id(car_bt$splits[[1]], TRUE)
+
+  expect_equal(colnames(res), c(colnames(mtcars), ".id"))
+
+  expect_error(
+    analysis(car_folds$splits[[1]]) %>%
+      add_resample_id(car_folds$splits[[1]], 7)
+  )
+  expect_error(
+    analysis(car_folds$splits[[1]]) %>%
+      add_resample_id(car_folds$splits[[1]], c(TRUE, TRUE))
+  )
+
+  expect_error(
+    analysis(car_folds$splits[[1]]) %>%
+      add_resample_id(car_folds$splits)
+  )
+
+  expect_error(
+    analysis(car_folds$splits[[1]]) %>%
+      as.matrix() %>%
+      add_resample_id(car_folds$splits[[1]])
+  )
 })
