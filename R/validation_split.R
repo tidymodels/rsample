a3429948ecf025949e23fd5d3fd88ad1430078ff
fbchow <fannybchow@gmail.com>
2020-02-11 22:22:45 -0800

    This

--- R/validation_split.R
+++ R/validation_split.R
@@ -4,7 +4,7 @@
 #'  the original data set to be used for analysis. All other data points are
 #'  added to the assessment set (to be used as the validation set).
 #' @details The `strata` argument causes the random sampling to be conducted
-#'  *within the stratification variable*. The can help ensure that the number of
+#'  *within the stratification variable*. This can help ensure that the number of
 #'  data points in the analysis data is equivalent to the proportions in the
 #'  original data set.
 #' @inheritParams vfold_cv
