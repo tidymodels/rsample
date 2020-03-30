1c627efb97fb0c94ff9693054dc457b1e891afca
topepo <mxkuhn@gmail.com>
2020-03-29 20:35:53 -0400

    export new_rset for #40

--- R/0_imports.R
+++ R/0_imports.R
@@ -4,7 +4,7 @@
 #' @importFrom tidyr gather unnest
 #' @importFrom dplyr select %>% bind_cols bind_rows arrange_ arrange full_join
 #' @importFrom dplyr mutate last ungroup group_by inner_join summarize do case_when
-#' @importFrom rlang !! is_call is_string enquo quos exec is_list
+#' @importFrom rlang !! is_call is_string enquo quos exec is_list abort warn
 #' @importFrom methods formalArgs
 #' @importFrom stats quantile setNames qnorm pnorm
 #' @importFrom tidyselect vars_select one_of
--- R/misc.R
+++ R/misc.R
@@ -17,9 +17,9 @@ dim_rset <- function(x, ...) {
   dims
 }
 
-names0 <- function (num, prefix = "x") {
+names0 <- function(num, prefix = "x") {
   if (num < 1)
-    stop("`num` should be > 0", call. = FALSE)
+    rlang::abort("`num` should be > 0")
   ind <- format(1:num)
   ind <- gsub(" ", "0", ind)
   paste0(prefix, ind)
@@ -36,46 +36,53 @@ add_class <- function(x, cls, at_end = TRUE) {
 strata_check <- function(strata, vars) {
   if (!is.null(strata)) {
     if (!is.character(strata) | length(strata) != 1)
-      stop("`strata` should be a single character value", call. = FALSE)
+      rlang::abort("`strata` should be a single character value")
     if (!(strata %in% vars))
-      stop(strata, " is not in `data`")
+      rlang::abort(strata, " is not in `data`")
   }
   invisible(NULL)
 }
 
-
-# `splits`` should be either a list or a tibble with a single column
-#  called "splits"
-# `ids`` should be either a character vector or a tibble with
-#  one or more columns that begin with "id"
+#' Constructor for new rset objects
+#' @param splits A list column of `rsplits` or a tibble with a single column
+#'  called "splits" with a list column of `rsplits`.
+#' @param ids A character vector or a tibble with one or more columns that
+#' begin with "id".
+#' @param attrib An optional named list of attributes to add to the object.
+#' @param subclass A character vector of subclasses to add.
+#' @return An `rset` object.
+#' @keywords internal
+#' @export
 new_rset <-  function(splits, ids, attrib = NULL,
                       subclass = character()) {
   stopifnot(is.list(splits))
   if (!is_tibble(ids)) {
     ids <- tibble(id = ids)
   } else {
-    if (!all(grepl("^id", names(ids))))
-      stop("The `ids` tibble column names should start with 'id'",
-           call. = FALSE)
+    if (!all(grepl("^id", names(ids)))) {
+        rlang::abort("The `ids` tibble column names should start with 'id'")
+    }
   }
   either_type <- function(x)
     is.character(x) | is.factor(x)
   ch_check <- vapply(ids, either_type, c(logical = TRUE))
-  if(!all(ch_check))
-    stop("All ID columns should be character or factor ",
-         "vectors.", call. = FALSE)
+  if (!all(ch_check)) {
+    rlang::abort("All ID columns should be character or factor vectors.")
+  }
 
   if (!is_tibble(splits)) {
     splits <- tibble(splits = splits)
   } else {
-    if(ncol(splits) > 1 | names(splits)[1] != "splits")
-      stop("The `splits` tibble should have a single column ",
-           "named `splits`.", call. = FALSE)
+    if (ncol(splits) > 1 | names(splits)[1] != "splits") {
+      rlang::abort(
+       "The `splits` tibble should have a single column named `splits`."
+      )
+    }
   }
 
-  if (nrow(ids) != nrow(splits))
-    stop("Split and ID vectors have different lengths.",
-         call. = FALSE)
+  if (nrow(ids) != nrow(splits)) {
+    rlang::abort("Split and ID vectors have different lengths.")
+  }
 
   # Create another element to the splits that is a tibble containing
   # an identifer for each id column so that, in isolation, the resample
@@ -87,15 +94,17 @@ new_rset <-  function(splits, ids, attrib = NULL,
   res <- bind_cols(splits, ids)
 
   if (!is.null(attrib)) {
-    if (any(names(attrib) == ""))
-      stop("`attrib` should be a fully named list.",
-           call. = FALSE)
-    for (i in names(attrib))
+    if (any(names(attrib) == "")) {
+      rlang::abort("`attrib` should be a fully named list.")
+    }
+    for (i in names(attrib)) {
       attr(res, i) <- attrib[[i]]
+    }
   }
 
-  if (length(subclass) > 0)
+  if (length(subclass) > 0) {
     res <- add_class(res, cls = subclass, at_end = FALSE)
+  }
 
   res
 }
