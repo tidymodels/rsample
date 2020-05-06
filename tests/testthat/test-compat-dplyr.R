6da89e5d866fcf3c5f4935d7ba95107c5e504ed1
DavisVaughan <davis@rstudio.com>
2020-05-06 17:42:54 -0400

    Add a few `mutate()` tests

--- tests/testthat/test-compat-dplyr.R
+++ tests/testthat/test-compat-dplyr.R
@@ -2,20 +2,18 @@
 # dplyr_reconstruct()
 
 test_that("reconstructing to an rset returns a bare tibble", {
-  for (i in seq_along(rset_subclasses)) {
-    x <- rset_subclasses[[i]]
+  for (x in rset_subclasses) {
     expect_s3_class_bare_tibble(dplyr_reconstruct(x, x))
     expect_identical(dplyr_reconstruct(x, x), rset_strip(x))
   }
 })
 
 test_that("reconstructing retains extra attributes", {
-  for (i in seq_along(rset_subclasses)) {
-    x1 <- rset_subclasses[[i]]
-    x2 <- x1
+  for (x in rset_subclasses) {
+    x2 <- x
     attr(x2, "foo") <- "bar"
 
-    result <- dplyr_reconstruct(x1, x2)
+    result <- dplyr_reconstruct(x, x2)
 
     expect_identical(attr(result, "foo"), "bar")
   }
@@ -25,8 +23,7 @@ test_that("reconstructing retains extra attributes", {
 # dplyr_col_modify()
 
 test_that("can add columns and retain rset class", {
-  for (i in seq_along(rset_subclasses)) {
-    x <- rset_subclasses[[i]]
+  for (x in rset_subclasses) {
     cols <- list(x = rep(1, vec_size(x)))
 
     result <- dplyr_col_modify(x, cols)
@@ -37,8 +34,7 @@ test_that("can add columns and retain rset class", {
 })
 
 test_that("modifying rset columns removes rset class", {
-  for (i in seq_along(rset_subclasses)) {
-    x <- rset_subclasses[[i]]
+  for (x in rset_subclasses) {
     cols <- list(splits = rep(1, vec_size(x)))
 
     result <- dplyr_col_modify(x, cols)
@@ -47,13 +43,32 @@ test_that("modifying rset columns removes rset class", {
     expect_identical(result$splits, cols$splits)
   }
 
-  for (i in seq_along(rset_subclasses)) {
-    x <- rset_subclasses[[i]]
+  for (x in rset_subclasses) {
     cols <- list(id = rep(1, vec_size(x)))
 
     result <- dplyr_col_modify(x, cols)
 
     expect_s3_class_bare_tibble(result)
     expect_identical(result$id, cols$id)
+  }
+})
+
+# ------------------------------------------------------------------------------
+# mutate()
+
+test_that("mutate() can keep rset class", {
+  for (x in rset_subclasses) {
+    expect_s3_class_rset(mutate(x, x = 1))
+    expect_identical(mutate(x, x = 1)$x, rep(1, vec_size(x)))
+  }
+})
+
+test_that("mutate() drops rset class if any rset columns are touched", {
+  for (x in rset_subclasses) {
+    expect_s3_class_bare_tibble(mutate(x, splits = 1))
+    expect_s3_class_bare_tibble(mutate(x, id = 1))
+
+    expect_identical(mutate(x, splits = 1)$splits, rep(1, vec_size(x)))
+    expect_identical(mutate(x, id = 1)$id, rep(1, vec_size(x)))
   }
 })
