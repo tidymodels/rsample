60ed9e17a5d71300c8574a6bc1200406b7161968
DavisVaughan <davis@rstudio.com>
2020-05-11 12:58:04 -0400

    More documentation

--- R/compat-dplyr.R
+++ R/compat-dplyr.R
@@ -7,6 +7,11 @@
 # - At the end of a call to `dplyr_row_slice()`
 # - See `?dplyr_reconstruct` for the full list.
 #
+# Because `dplyr_reconstruct()` is called at the end of `dplyr_col_modify()`
+# and `dplyr_row_slice()`, we don't need methods for them. The default methods
+# in dplyr do the right thing automatically, and then our reconstruction
+# method decides whether or not the result should still be an rset.
+#
 # The implementation for rsample is the same as `vec_restore()`. Generally
 # it will fall back to reconstructing a bare tibble, unless the rset structure
 # is still completely intact. This happens when rset specific rows and columns
--- R/compat-vctrs.R
+++ R/compat-vctrs.R
@@ -1,10 +1,6 @@
 #' @import vctrs
 NULL
 
-# The cast, ptype2, and restore methods are all the same here, but we
-# can't use inheritance because vctrs doesn't allow it in the ptype2 and
-# cast methods.
-
 # `vec_restore()`
 #
 # Called at the end of `vec_slice()` and `vec_ptype()` after all slicing has
@@ -21,6 +17,52 @@ NULL
 # essentially takes a 0-row slice of the rset, and then calls `vec_restore()`.
 # Because the row structure has been modified, we return a bare tibble from
 # `vec_restore.<rset-subclass>()`.
+#
+# Currently `vec_restore()` uses inheritance in vctrs, but I don't expect this
+# to be the case in the future. For that reason, I use explicit methods for
+# each individual rset subclass, rather than implementing `vec_restore.rset()`.
+
+
+# `vec_ptype2()`
+#
+# The purpose of `vec_ptype2()` methods is generally to determine the type
+# of the output in operations like `vec_c()` or `vec_rbind()`. However, this
+# implementation does not use any custom `vec_ptype2()` methods at all. This is
+# explained below.
+#
+# `vec_ptype2()` internally works by calling `vec_ptype()` on both `x` and `y`,
+# and then looking up the common type of those two ptypes. Generally, the
+# ptype of a vector `x` returned from `vec_ptype()` has the same class as `x`.
+# However, for rsample objects it makes more sense for the ptype of an rset to
+# be a bare tibble. The reason for this is because a ptype of a data frame is
+# generally a 0-row slice. However for rsample rsets this doesn't make sense
+# (you can't have 0 rows in a 10-fold cv rset), so instead we return a bare
+# tibble as the ptype.
+# So when `vec_ptype2()` is called on an rset, that rset is downgraded to a
+# bare tibble _before_ the search for a `vec_ptype2()` method occurs. This
+# means that it will never find a ptype2 method such as
+# `vec_ptype2.bootstraps.tbl_df()`, because the <bootstraps> will become a
+# tbl_df first. This means that we rely entirely on the `tbl_df` ptype2 methods,
+# which are already implemented in vctrs to return another tbl_df.
+#
+# The implications of this are that whenever a rset subclass is combined with
+# another rset subclass or bare tibble, the resulting common type is always
+# another bare tibble. So if you `vec_c(<bootstraps>, <tbl_df>)` the result
+# will always be a tibble, never a bootstraps object. This makes sense because
+# you might be adding rows, which would invalidate the structure of the
+# bootstraps object.
+
+
+# `vec_cast()`
+#
+# The `vec_cast()` methods for rset objects really only serve 1 purpose. They
+# cast an rset subclass to a tibble or data frame. The cast to tibble is most
+# useful. Most of the operations in vctrs work by finding a common type
+# with `vec_ptype2()`, and then casting all of the inputs to that common type.
+# Because `vec_ptype2()` returns a bare tibble anytime a rset-subclass is
+# involved, we will always be casting the rset subclass to a tibble.
+# The cast method uses `vctrs::tib_cast()`, which always returns a bare tibble
+# with all of the data in `x` cast to the type of `to`.
 
 # ------------------------------------------------------------------------------
 # bootstraps
