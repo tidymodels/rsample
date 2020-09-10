1ac7029139c3228d560e899c523d3f3814b6b8ac
DavisVaughan <davis@rstudio.com>
2020-09-10 17:19:07 -0400

    Add tests for `manual_rset()`

---
+++ tests/testthat/test-manual.R
@@ -0,0 +1,36 @@
+test_that("can create a manual rset", {
+  df <- data.frame(x = c(1, 2, 3, 4))
+
+  indices <- list(
+    list(analysis = 1L, assessment = 2L),
+    list(analysis = 3L, assessment = 4L)
+  )
+
+  splits <- lapply(indices, make_splits, data = df)
+
+  rset <- manual_rset(splits, c("Split 1", "Split 2"))
+
+  expect_s3_class(rset, c("manual_rset", "rset"))
+  expect_identical(rset$id, c("Split 1", "Split 2"))
+})
+
+test_that("can use analysis/assessment functions", {
+  df <- data.frame(x = c(1, 2, 3))
+  indices <- list(analysis = c(1L, 2L), assessment = 3L)
+  splits <- list(make_splits(indices, df))
+
+  rset <- manual_rset(splits, "Split 1")
+
+  expect_identical(analysis(rset$splits[[1]]), df[1:2, 1, drop = FALSE])
+  expect_identical(assessment(rset$splits[[1]]), df[3, 1, drop = FALSE])
+})
+
+test_that("`pretty()` works", {
+  df <- data.frame(x = c(1, 2, 3))
+  indices <- list(analysis = c(1L, 2L), assessment = 3L)
+  splits <- list(make_splits(indices, df))
+
+  rset <- manual_rset(splits, "Split 1")
+
+  expect_identical(pretty(rset), "Manual resampling")
+})
