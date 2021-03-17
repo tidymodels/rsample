9404eef24766254e4723e5afaae48d2b2538cdfe
Hannah Frick <hfrick@users.noreply.github.com>
2021-03-17 10:56:17 +0000

    Update R/misc.R
    Co-authored-by: Julia Silge <julia.silge@gmail.com>

--- R/misc.R
+++ R/misc.R
@@ -44,7 +44,7 @@ strata_check <- function(strata, data) {
       rlang::abort("`strata` should be a single character value.")
     }
     if (inherits(data[, strata], "Surv")) {
-      rlang::abort("`strata` cannot be a `Surv` object. Use the time or event indicator directly.")
+      rlang::abort("`strata` cannot be a `Surv` object. Use the time or event variable directly.")
     }
     if (!(strata %in% names(data))) {
       rlang::abort(strata, " is not in `data`.")
