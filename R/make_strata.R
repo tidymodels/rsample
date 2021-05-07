7cb8c6604964a4797cba53c8362c48fc916694ab
Julia Silge <julia.silge@gmail.com>
2021-05-07 13:00:25 -0600

    Count unique values _without_ NA values

--- R/make_strata.R
+++ R/make_strata.R
@@ -71,7 +71,7 @@
 make_strata <- function(x, breaks = 4, nunique = 5, pool = .1, depth = 20) {
 
   default_pool <- 0.1
-  num_vals <- unique(x)
+  num_vals <- unique(na.omit(x))
   n <- length(x)
   if (length(num_vals) <= nunique | is.character(x) | is.factor(x)) {
     x <- factor(x)
