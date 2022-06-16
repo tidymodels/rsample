d1ba15cd693fb1406a08a94c167125736938cc32
Julia Silge <julia.silge@gmail.com>
2022-06-16 09:10:28 -0600

    Remove old compat dplyr (#306)
    * Move "old" dplyr compat tests to join new
    
    * Remove old pre-1.0.0 dplyr handling
    
    * Rearrange tests to be in more sensible order

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,7 @@
 # nocov start
 
 .onLoad <- function(libname, pkgname) {
-  if (dplyr_pre_1.0.0()) {
-    vctrs::s3_register("dplyr::mutate", "rset", method = mutate_rset)
-    vctrs::s3_register("dplyr::arrange", "rset", method = arrange_rset)
-    vctrs::s3_register("dplyr::filter", "rset", method = filter_rset)
-    vctrs::s3_register("dplyr::rename", "rset", method = rename_rset)
-    vctrs::s3_register("dplyr::select", "rset", method = select_rset)
-    vctrs::s3_register("dplyr::slice", "rset", method = slice_rset)
-  } else {
-    vctrs::s3_register("dplyr::dplyr_reconstruct", "rset", method = dplyr_reconstruct_rset)
-  }
+  vctrs::s3_register("dplyr::dplyr_reconstruct", "rset", method = dplyr_reconstruct_rset)
 }
 
 # nocov end
