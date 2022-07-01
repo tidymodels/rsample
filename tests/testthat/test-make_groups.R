da14bf78ce6447b0b83f180d4cbd503594e4de6d
Michael Mahoney <mike.mahoney.218@gmail.com>
2022-07-01 19:10:18 -0400

    Set seed when generating rset_subclasses (#332)
    * Set seed when generating rset_subclasses
    
    * Move withr to Suggests

--- tests/testthat/test-make_groups.R
+++ tests/testthat/test-make_groups.R
@@ -1,5 +1,5 @@
 test_that("grouped variants have the same classes as nongrouped outputs", {
-
+  skip_if_not(rlang::is_installed("withr"))
   grouped_variants <- grep("^group_", names(rset_subclasses), value = TRUE)
 
   for (x in grouped_variants) {
