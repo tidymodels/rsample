3533894d3d3f8426054448bd9a8c8889f4ca7064
Mike Mahoney <mike.mahoney.218@gmail.com>
2022-07-05 18:48:40 -0400

    Test a bit more thoroughly

--- tests/testthat/_snaps/misc.md
+++ tests/testthat/_snaps/misc.md
@@ -22,3 +22,39 @@
       Error in `reverse_splits()`:
       ! Permutations cannot have their splits reversed
 
+---
+
+    Code
+      reverse_splits(1)
+    Condition
+      Error in `reverse_splits()`:
+      ! `x` must be either an `rsplit` or an `rset` object
+
+---
+
+    Code
+      reverse_splits(permutes)
+    Condition
+      Error in `reverse_splits()`:
+      ! Permutations cannot have their splits reversed
+
+---
+
+    Code
+      reverse_splits(permutes$splits[[1]])
+    Condition
+      Error in `reverse_splits()`:
+      ! Permutations cannot have their splits reversed
+
+# reshuffle_rset is working
+
+    Cannot reshuffle this rset (`attr(rset, 'strata')` is `TRUE`, not a column identifier)
+
+---
+
+    `manual_rset` objects cannot be reshuffled
+
+---
+
+    `rset` must be an rset object
+
--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -57,12 +57,18 @@ test_that("reshuffle_rset is working", {
     )
   }
 
+  # Check to make sure that stratification,
+  # with non-default arguments,
+  # is supported by reshuffled_resample
+
+  # Select any function in rset_subclasses with a strata argument
   supports_strata <- purrr::map_lgl(
     names(supported_subclasses),
     ~ any(names(formals(.x)) == "strata")
   )
   supports_strata <- names(supported_subclasses)[supports_strata]
   supports_strata <- supported_subclasses[supports_strata]
+
   for (i in seq_along(supports_strata)) {
     set.seed(123)
     resample <- do.call(
@@ -71,11 +77,109 @@ test_that("reshuffle_rset is working", {
         data = test_data(),
         strata = "y",
         breaks = 2,
-        pool = 0.1
+        pool = 0.2
       )
     )
     set.seed(123)
     reshuffled_resample <- reshuffle_rset(resample)
     expect_identical(resample, reshuffled_resample)
   }
+})
+test_that("reverse_splits is working", {
+  skip_if_not(rlang::is_installed("withr"))
+
+  reversable_subclasses <- setdiff(names(rset_subclasses), "permutations")
+  reversable_subclasses <- rset_subclasses[reversable_subclasses]
+  for (x in reversable_subclasses) {
+
+    set.seed(123)
+    rev_x <- reverse_splits(x)
+    expect_identical(analysis(x$splits[[1]]), assessment(rev_x$splits[[1]]))
+    expect_identical(assessment(x$splits[[1]]), analysis(rev_x$splits[[1]]))
+    expect_identical(class(x), class(rev_x))
+    expect_identical(class(x$splits[[1]]), class(rev_x$splits[[1]]))
+
+  }
+
+  expect_snapshot(
+    reverse_splits(1),
+    error = TRUE
+  )
+
+  permutes <- permutations(mtcars, cyl)
+
+  expect_snapshot(
+    reverse_splits(permutes),
+    error = TRUE
+  )
+
+  expect_snapshot(
+    reverse_splits(permutes$splits[[1]]),
+    error = TRUE
+  )
+
+})
+
+test_that("reshuffle_rset is working", {
+
+  skip_if_not(rlang::is_installed("withr"))
+  supported_subclasses <- rset_subclasses[
+    setdiff(names(rset_subclasses), c("manual_rset"))
+  ]
+
+  # Reshuffling with the same seed, in the same order,
+  # should recreate the same objects
+  out <- withr::with_seed(
+    123,
+    lapply(
+      supported_subclasses,
+      reshuffle_rset
+    )
+  )
+
+  for (i in seq_along(supported_subclasses)) {
+    expect_identical(
+      out[[i]],
+      supported_subclasses[[i]]
+    )
+  }
+
+  # Check to make sure that stratification,
+  # with non-default arguments,
+  # is supported by reshuffled_resample
+
+  # Select any function in rset_subclasses with a strata argument:
+  supports_strata <- purrr::map_lgl(
+    names(supported_subclasses),
+    ~ any(names(formals(.x)) == "strata")
+  )
+  supports_strata <- names(supported_subclasses)[supports_strata]
+
+  for (i in seq_along(supports_strata)) {
+    # Fit those functions with non-default arguments:
+    set.seed(123)
+    resample <- do.call(
+      supports_strata[i],
+      list(
+        data = test_data(),
+        strata = "y",
+        breaks = 2,
+        pool = 0.2
+      )
+    )
+    # Reshuffle them under the same seed to ensure they're identical
+    set.seed(123)
+    reshuffled_resample <- reshuffle_rset(resample)
+    expect_identical(resample, reshuffled_resample)
+  }
+
+  resample <- vfold_cv(mtcars, strata = cyl)
+  attr(resample, "strata") <- TRUE
+
+  expect_snapshot_error(reshuffle_rset(resample))
+
+  expect_snapshot_error(reshuffle_rset(rset_subclasses[["manual_rset"]]))
+
+  expect_snapshot_error(reshuffle_rset(rset_subclasses[["manual_rset"]]$splits[[1]]))
+
 })
