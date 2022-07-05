2ea729992253eb010d103e5d62f150371f269d85
Michael Mahoney <mike.mahoney.218@gmail.com>
2022-07-05 13:01:10 -0400

    Auto-generate vctrs (#326)
    * Auto-generate vctrs
    
    * Redocument
    
    * Move to inst, add release questions
    
    * Generate separate files
    
    * Re-run with all classes
    
    * Rename to zzz prefix

---
+++ inst/generate_vctrs.R
@@ -0,0 +1,20 @@
+# This file generates helpers for compatibility with vctrs
+# and is not a part of the rsample package proper
+
+devtools::load_all()
+read_utf8 <- function(x) base::readLines(x, encoding = "UTF-8", warn = FALSE)
+
+template <- read_utf8("inst/vctrs_template.R")
+
+for (name in names(rset_subclasses)) {
+  generated_template <- whisker::whisker.render(template)
+  generated_template <- c(
+    "# This file was generated, do not edit by hand",
+    "# Please edit inst/generate_vctrs.R instead",
+    generated_template
+  )
+  writeLines(
+    generated_template,
+    file.path("R", paste0("zzz-compat-vctrs-", name, ".R"))
+  )
+}
