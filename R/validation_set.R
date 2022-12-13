297680455659f828472baa0e9d7a3064cad7e582
Hannah Frick <hannah@rstudio.com>
2022-12-13 18:55:48 +0000

    add `validation_set()`

---
+++ R/validation_set.R
@@ -0,0 +1,77 @@
+#' Create a Validation Split for Tuning
+#'
+#' @param x An object of class `initial_validation_split`, such as resulting
+#' from [initial_validation_split()] or [group_initial_validation_split()].
+#' @inheritParams rlang::args_dots_empty
+#'
+#' @return An tibble with classes `validation_set`, `rset`, `tbl_df`, `tbl`, and
+#'  `data.frame`. The results include a column for the data split object and a
+#'  column called `id` that has a character string with the resample identifier.
+#' @export
+#'
+#' @examples
+#' set.seed(1353)
+#' car_split <- initial_validation_split(mtcars)
+#' car_set <- validation_set(car_split)
+validation_set <- function(x, ...) {
+  rlang::check_dots_empty()
+
+  train_and_val <- rbind(
+    training(x),
+    validation(x)
+  )
+  n_train <- length(x$train_id)
+
+  val_split <- rsplit(
+    data = train_and_val,
+    in_id = seq_len(n_train),
+    out_id = NA
+  )
+
+  # this is same class as via the alternative `validation_split()`
+  class(val_split) <- c("val_split", "rsplit")
+
+  val_att <- attr(x, "val_att")
+
+  new_rset(
+    splits = list(val_split),
+    ids = "validation",
+    attrib = val_att,
+    subclass = c("validation_set", "rset")
+  )
+}
+
+# accessor methods for the rsplit -----------------------------------------
+
+#' @rdname validation_set
+#' @export
+analysis.val_split <- function(x, ...) {
+  as.data.frame(x, data = "analysis", ...)
+}
+
+#' @rdname validation_set
+#' @export
+assessment.val_split <- function(x, ...) {
+  as.data.frame(x, data = "assessment", ...)
+}
+
+#' @rdname validation_set
+#' @export
+training.val_split <- function(x, ...) {
+  analysis(x, ...)
+}
+
+#' @rdname validation_set
+#' @export
+validation.val_split <- function(x, ...) {
+  assessment(x, ...)
+}
+
+#' @rdname validation_set
+#' @export
+testing.val_split <- function(x, ...) {
+  rlang::abort(
+    "The testing data is not part of the validation set.", # TODO change error message?
+    i = "It is part of the result of `initial_validation_split()`."
+  )
+}
---
+++ tests/testthat/test-validation_set.R
@@ -0,0 +1,42 @@
+test_that("basic set", {
+  dat1 <- data.frame(a = 1:20, b = letters[1:20], c = rep(1:4, 5))
+
+  set.seed(11)
+  initial_val_split <- initial_validation_split(dat1, prop = c(0.6, 0.2))
+  val_set <- validation_set(initial_val_split)
+  train_and_val <- dplyr::bind_rows(
+    training(initial_val_split),
+    validation(initial_val_split)
+  )
+
+  expect_s3_class(val_set, "rset")
+  expect_s3_class(val_set, "validation_set")
+
+  set_sizes <- dim_rset(val_set)
+  expect_true(all(set_sizes$analysis == 12))
+  expect_true(all(set_sizes$assessment == 4))
+
+  expect_equal(length(val_set$splits), 1)
+  expect_equal(
+    val_set$splits[[1]]$data,
+    train_and_val,
+    ignore_attr = "row.names"
+  )
+
+  overlap_train_val <- intersect(val_set$splits[[1]]$in_ind)
+  expect_equal(overlap_train_val, NULL)
+})
+
+test_that("accessor functions for `val_split`", {
+  dat1 <- data.frame(a = 1:20, b = letters[1:20], c = rep(1:4, 5))
+
+  set.seed(11)
+  initial_val_split <- initial_validation_split(dat1, prop = c(0.6, 0.2))
+  val_split <- validation_set(initial_val_split) %>% get_rsplit(1)
+
+  expect_equal(analysis(val_split), training(initial_val_split))
+  expect_equal(training(val_split), training(initial_val_split))
+  expect_equal(assessment(val_split), validation(initial_val_split))
+  expect_equal(validation(val_split), validation(initial_val_split))
+  expect_snapshot(error = TRUE, testing(val_split))
+})
