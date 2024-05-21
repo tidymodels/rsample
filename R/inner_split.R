c966979090cbfa5ace6ab3ce3b9823b9a80e5cd1
Hannah Frick <hannah@posit.co>
2024-05-21 14:50:47 +0100

    add `inner_split()` method for vfold cv

--- R/inner_split.R
+++ R/inner_split.R
@@ -56,3 +56,63 @@ inner_split.grouped_mc_split <- function(x, split_args, ...) {
   split_inner <- add_class(split_inner, class_inner)
   split_inner
 }
+
+
+# vfold ------------------------------------------------------------------
+
+#' @rdname inner_split
+#' @export
+inner_split.vfold_split <- function(x, split_args, ...) {
+  check_dots_empty() 
+
+  analysis_set <- analysis(x)
+  
+  # TODO should this be done outside of rsample, 
+  # in workflows or tune?
+  if (is.null(split_args$prop)) {
+    split_args$prop <- 1 - 1/split_args$v
+  }
+  # use mc_splits for a random split
+  split_inner <- mc_splits(
+    analysis_set, 
+    prop = split_args$prop, 
+    times = 1,
+    strata = split_args$strata, 
+    breaks = split_args$breaks, 
+    pool = split_args$pool
+  )
+  split_inner <- split_inner$splits[[1]]
+
+  class_inner <- paste0(class(x)[1], "_inner")
+  class(split_inner) <- c(class_inner, class(x))
+  split_inner
+}
+
+#' @rdname inner_split
+#' @export
+inner_split.group_vfold_split <- function(x, split_args, ...) {
+  check_dots_empty() 
+
+  analysis_set <- analysis(x)
+
+  # TODO should this be done outside of rsample, 
+  # in workflows or tune?
+  if (is.null(split_args$prop)) {
+    split_args$prop <- 1 - 1/split_args$v
+  }
+  
+  # use group_mc_splits for a random split
+  split_inner <- group_mc_splits(
+    analysis_set, 
+    group = split_args$group,
+    prop = split_args$prop, 
+    times = 1,
+    strata = split_args$strata, 
+    pool = split_args$pool
+  )
+  split_inner <- split_inner$splits[[1]]
+
+  class_inner <- paste0(class(x)[1], "_inner")
+  class(split_inner) <- c(class_inner, class(x))
+  split_inner
+}
--- tests/testthat/test-inner_split.R
+++ tests/testthat/test-inner_split.R
@@ -54,3 +54,60 @@ test_that("group_mc_split", {
     ignore_attr = "row.names"
   )
 })
+
+
+# vfold ------------------------------------------------------------------
+
+test_that("vfold_split", {
+  set.seed(11)
+  r_set <- vfold_cv(warpbreaks, v = 5)
+  split_args <- get_split_args(r_set)
+  r_split <- get_rsplit(r_set, 1)
+
+  isplit <- inner_split(r_split, split_args)
+
+  expect_identical(
+    isplit$data,
+    analysis(r_split)
+  )
+
+  expect_identical(
+    analysis(isplit),
+    isplit$data[isplit$in_id, ],
+    ignore_attr = "row.names"
+  )
+  expect_identical(
+    assessment(isplit),
+    isplit$data[isplit$out_id, ],
+    ignore_attr = "row.names"
+  )
+})
+
+test_that("group_vfold_split", {
+  skip_if_not_installed("modeldata")
+
+  data(ames, package = "modeldata", envir = rlang::current_env())
+
+  set.seed(11)
+  r_set <- group_vfold_cv(ames, "MS_SubClass")
+  split_args <- get_split_args(r_set)
+  r_split <- get_rsplit(r_set, 1)
+
+  isplit <- inner_split(r_split, split_args)
+
+  expect_identical(
+    isplit$data,
+    analysis(r_split)
+  )
+
+  expect_identical(
+    analysis(isplit),
+    isplit$data[isplit$in_id, ],
+    ignore_attr = "row.names"
+  )
+  expect_identical(
+    assessment(isplit),
+    isplit$data[isplit$out_id, ],
+    ignore_attr = "row.names"
+  )
+})
