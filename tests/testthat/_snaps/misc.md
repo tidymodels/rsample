cf5f98cfdd4c1f2117095de8695dfcca6c2f7165
Hannah Frick <hannah@posit.co>
2024-09-18 12:45:16 +0100

    split out `reverse_splits`

--- tests/testthat/_snaps/misc.md
+++ tests/testthat/_snaps/misc.md
@@ -1,27 +1,3 @@
-# reverse_splits is working
-
-    Code
-      reverse_splits(1)
-    Condition
-      Error in `reverse_splits()`:
-      ! `x` must be either an <rsplit> or an <rset> object.
-
----
-
-    Code
-      reverse_splits(permutes)
-    Condition
-      Error in `reverse_splits()`:
-      ! Permutations cannot have their splits reversed.
-
----
-
-    Code
-      reverse_splits(permutes$splits[[1]])
-    Condition
-      Error in `reverse_splits()`:
-      ! Permutations cannot have their splits reversed.
-
 # reshuffle_rset is working
 
     Code
---
+++ tests/testthat/test-reverse_splits.R
@@ -0,0 +1,34 @@
+test_that("reverse_splits is working", {
+  skip_if_not_installed("withr")
+
+  reversable_subclasses <- setdiff(names(rset_subclasses), "permutations")
+  reversable_subclasses <- rset_subclasses[reversable_subclasses]
+  for (x in reversable_subclasses) {
+
+    set.seed(123)
+    rev_x <- reverse_splits(x)
+    expect_identical(analysis(x$splits[[1]]), assessment(rev_x$splits[[1]]))
+    expect_identical(assessment(x$splits[[1]]), analysis(rev_x$splits[[1]]))
+    expect_identical(class(x), class(rev_x))
+    expect_identical(class(x$splits[[1]]), class(rev_x$splits[[1]]))
+
+  }
+
+  expect_snapshot(
+    reverse_splits(1),
+    error = TRUE
+  )
+
+  permutes <- permutations(mtcars, cyl)
+
+  expect_snapshot(
+    reverse_splits(permutes),
+    error = TRUE
+  )
+
+  expect_snapshot(
+    reverse_splits(permutes$splits[[1]]),
+    error = TRUE
+  )
+
+})
